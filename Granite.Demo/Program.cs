e9fbc70ffe1bdb87efbc5bf21c74e23a175910e1
andreijeman <andrei.jeman3@gmail.com>
2025-11-29 01:36:10 +0200

    feat: add Granite.Demo project

---
+++ Granite.Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Granite.Drawing.Geometry.Primitives;
+using Granite.Drawing.Outputs;
+using Granite.Drawing.Visual.Elements;
+using Granite.Drawing.Visual.Primitives;
+
+var box = new ColorBox
+{
+    Color = new Color("FF00FF"),
+    Bounds = new Rect(0, 0, 8, 8)
+};
+
+var output = new AnsiOutput();
+
+output.Draw(box);
